58e3b17923b169090e964770483660b65fb7d691
Maje53 <71787556+Maje53@users.noreply.github.com>
2026-03-07 02:20:18 +0300

    Resolve null pointer exception in user service

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T23:20:11.447Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-06T23:20:19.041Z
+Commit: Resolve null pointer exception in user service
