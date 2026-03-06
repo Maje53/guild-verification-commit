1b785ff60ff8007f90a2a299d4dbe9269dfe0649
Maje53 <71787556+Maje53@users.noreply.github.com>
2026-03-07 02:22:37 +0300

    Resolve null pointer exception in user service

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T23:21:46.017Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-06T23:22:37.277Z
+Commit: Resolve null pointer exception in user service
