d0a22862ed36f1b186f12dace06b0d39c0e411c5
Maje53 <71787556+Maje53@users.noreply.github.com>
2026-03-07 02:26:56 +0300

    Resolve null pointer exception in user service

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T23:26:53.422Z
-Commit: Fix database connection timeout
+Timestamp: 2026-03-06T23:26:56.659Z
+Commit: Resolve null pointer exception in user service
