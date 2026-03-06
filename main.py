0c259cf25f93f255e69e552cc9c1b2cdae56ebad
Maje53 <71787556+Maje53@users.noreply.github.com>
2026-03-07 02:20:43 +0300

    Fix broken links in documentation

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T23:19:50.111Z
-Commit: Patch security vulnerability in authentication
+Timestamp: 2026-03-06T23:20:43.412Z
+Commit: Fix broken links in documentation
