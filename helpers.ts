972205c08ade35424f755c32d184570b47d612c5
Maje53 <71787556+Maje53@users.noreply.github.com>
2026-03-07 02:21:10 +0300

    Fix memory leak in event handlers

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T23:21:08.996Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-03-06T23:21:10.556Z
+Commit: Fix memory leak in event handlers
