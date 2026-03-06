ac7d2b02b7abbe68cb48f5daea3ff0efb15369a8
Maje53 <71787556+Maje53@users.noreply.github.com>
2026-03-07 02:22:02 +0300

    Fix CSS styling bug on mobile devices

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T23:21:10.556Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2026-03-06T23:22:02.490Z
+Commit: Fix CSS styling bug on mobile devices
