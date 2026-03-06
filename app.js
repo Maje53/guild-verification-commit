f15b49c711deea402befad773130e0388c30e6b3
Maje53 <71787556+Maje53@users.noreply.github.com>
2026-03-07 02:20:39 +0300

    Fix broken links in documentation

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T23:20:38.842Z
-Commit: Implement user profile customization
+Timestamp: 2026-03-06T23:20:40.007Z
+Commit: Fix broken links in documentation
