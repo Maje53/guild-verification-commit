6590cbe2848c68b82296bb8bde7ad1f3081a210c
Maje53 <71787556+Maje53@users.noreply.github.com>
2026-03-07 02:22:40 +0300

    Implement user profile customization

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T23:22:22.567Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-03-06T23:22:40.779Z
+Commit: Implement user profile customization
